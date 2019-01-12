90d9c3a749e0c43d13ad82b5b085c357698ab326
xinyuwang <xinyucs@gmail.com>
2019-01-12 19:00:41 +0800

    Add captcha

--- content.js
+++ content.js
@@ -1,5 +1,79 @@
 ﻿//t could be others, fine with 1.
-var g_captcha_url = "https://www.ticketing.highspeed.mtr.com.hk/its/captcha.jpg?t=1";
+var g_captchaUrl = "https://www.ticketing.highspeed.mtr.com.hk/its/captcha.jpg?t=1";
+
+//Ruokuai account information
+var g_codeUser = "wmw1989";
+var g_codePsw = "testtest";
+var g_apiUrl = "http://api.ruokuai.com/create.json";
+var g_softKey = "918c2c4e4d494ee491d197150fcdd01c";
+var g_softID = 22692;
+var g_typeID = 3000;
+
+//submit form
+var submit = new Promise((resolve, reject) => {
+
+    let frm = $('#query_form');
+    frm.submit((e) => {
+        e.preventDefault();
+
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            data: frm.serialize(),
+            success: function (data) {
+                resolve(data);
+            },
+            error: function (data) {
+                reject(data);
+            }
+        });
+
+    });
+
+});
+
+//handle captcha
+var captcha = new Promise((resolve, reject) => {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+
+            var fd = new FormData();
+            fd.append('username', g_codeUser);
+            fd.append('password', g_codePsw);
+            fd.append('typeid', g_typeID);
+            fd.append('softid', g_softID);
+            fd.append('softkey', g_softKey);
+            fd.append('image', this.response);
+
+            $.ajax({
+                type: 'POST',
+                accepts: 'multipart/form-data',
+                url: g_apiUrl,
+                timeout: 600000,
+                data: fd,
+                processData: false,
+                contentType: false,
+
+                success: function (data) {
+                    resolve(data["Result"]);
+                },
+
+                error: function (data) {
+                    reject(data);
+                }
+            });
+
+        }
+    };
+
+    xhr.open('GET', g_captchaUrl);
+    xhr.responseType = 'blob';
+    xhr.send();
+
+});
 
 (() => {
 
@@ -37,34 +111,12 @@ var g_captcha_url = "https://www.ticketing.highspeed.mtr.com.hk/its/captcha.jpg?
     $('.searchbg_t').append(ui);
 
     //Handle captcha
+    captcha().then((data) => {
+        alert(data);
+    });
 
 
 })();
-
-
-
-let submit = new Promise((resolve, reject) => {
-
-    let frm = $('#query_form');
-    frm.submit((e) => {
-        e.preventDefault();
-
-        $.ajax({
-            type: frm.attr('method'),
-            url: frm.attr('action'),
-            data: frm.serialize(),
-            success: function (data) {
-                resolve(data);
-            },
-            error: function (data) {
-                reject(data);
-            },
-        });
-
-    });
-
-});
-
 
 
 //listen the event from background
@@ -72,83 +124,8 @@ let submit = new Promise((resolve, reject) => {
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 
     /* If the received message has the expected format... */
-    if (msg.text && msg.text == "set") {
-
-        _store = msg.store;
-
-        InitConfig(msg.store);
-
-        if (msg.text == "login") {
-
-
-            //if need auto code
-            var bAutoCode = msg.bAutoCode;
-            var bLocalCode = msg.bLocalCode;
-            var sLocalCodeServer = msg.localCodeURL;
-            var sLocalCoder = msg.localCoder;
-
-            if (bAutoCode == 'Auto' || bLocalCode == 'Auto') {
-                //For write the code
-                intervalID = setInterval(function () {
-
-                    var codeDiv = document.getElementById('captcha');
-                    if (codeDiv) {
-                        var code_src = codeDiv["src"];
-                        if (code_src.length > 10) {
-
-                            clearInterval(intervalID);
-
-                            var xhr = new XMLHttpRequest();
-                            xhr.onreadystatechange = function () {
-                                if (this.readyState == 4 && this.status == 200) {
-
-                                    var bSubmit = true;
-
-                                    //Use remote code
-                                    if (bAutoCode == 'Auto') {
-                                        var fd = new FormData();
-                                        fd.append('username', autoCodeUser);
-                                        fd.append('password', autoCodePsw);
-                                        fd.append('typeid', autoTypeID);
-                                        fd.append('softid', autoSoftID);
-                                        fd.append('softkey', autoCodeKey);
-                                        fd.append('image', this.response);
-
-
-                                        $.ajax({
-                                            type: 'POST',
-                                            accepts: 'multipart/form-data',
-                                            url: autoCodeUrl,
-                                            timeout: 600000,
-                                            data: fd,
-                                            processData: false,
-                                            contentType: false
-                                        }).done(function (data) {
-
-                                            DoLog("Get the AutoCode = " + data["Result"]);
-
-                                            $('#captchaInput').val(data["Result"]);
-
-                                        });
-                                    }
-
-
-                                }
-                            }
-                            xhr.open('GET', code_src);
-                            xhr.responseType = 'blob';
-                            xhr.send();
-
-                        }
-
-                    }
-
-                }, 100);
-
-            }
-
-        }
-
+    if (msg.text && msg.text === "set") {
+        
         sendResponse(msg.text);
     }
 });
